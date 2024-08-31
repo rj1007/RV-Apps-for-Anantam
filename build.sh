3718675b4f7276a56476c2d0458a2a5529efe231
RJ <rjaakash07@gmail.com>
2024-08-31 17:07:50 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -178,14 +178,14 @@ wait
 rm -rf temp/tmp.*
 if [ -z "$(ls -A1 ${BUILD_DIR})" ]; then abort "All builds failed."; fi
 
-log "\nInstall [Microg](https://github.com/ReVanced/GmsCore/releases) for non-root YouTube and YT Music APKs"
-log "Use [zygisk-detach](https://github.com/j-hc/zygisk-detach) to detach root ReVanced YouTube and YT Music from Play Store"
-log "\n[revanced-magisk-module](https://github.com/j-hc/revanced-magisk-module)\n"
+log "\n- To use YouTube and YT Music without root, install [microG](https://github.com/ReVanced/GmsCore/releases/latest) or [MicroG RE](https://github.com/WSTxda/MicroG-RE/releases/latest)"
+log "- To detach RV Apps from Play Store, use [zygisk-detach](https://github.com/j-hc/zygisk-detach)"
+log "\n**Dependencies** 👇"
 log "$(cat $TEMP_DIR/*-rv/changelog.md)"
 
 SKIPPED=$(cat $TEMP_DIR/skipped 2>/dev/null || :)
 if [ -n "$SKIPPED" ]; then
-	log "\nSkipped:"
+	log "\n**Skipped** 👇"
 	log "$SKIPPED"
 fi
 
