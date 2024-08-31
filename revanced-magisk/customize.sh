866f8a865f669e3426eeec3947017669b9ba1dc4
RJ <rjaakash07@gmail.com>
2024-08-31 16:58:56 +0530

    Update customize.sh

--- revanced-magisk/customize.sh
+++ revanced-magisk/customize.sh
@@ -128,5 +128,5 @@ ui_print "* Cleanup"
 rm -rf "${MODPATH:?}/bin" "$MODPATH/$PKG_NAME.apk"
 
 ui_print "* Done"
-ui_print "  by j-hc (github.com/j-hc)"
+ui_print "  by RJ (github.com/rjaakash)"
 ui_print " "
